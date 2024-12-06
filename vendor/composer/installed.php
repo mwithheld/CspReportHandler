830c1f80e40b17b3354533f8d71d8ac8abaa6bec
mwithheld <796986+mwithheld@users.noreply.github.com>
2024-12-06 10:08:42 -0800

    composer init and add php-curl pinned to version 10 for PHP7.3 compat

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'uvic/csp-report-handler',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php-curl-class/php-curl-class' => array(
+            'pretty_version' => '10.0.1',
+            'version' => '10.0.1.0',
+            'reference' => '3fbd244703d126fbabef6ef1904135ae79796bf9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-curl-class/php-curl-class',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'uvic/csp-report-handler' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
